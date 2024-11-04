cb51642ae8af6903d63d15771b3a47cb145ba422
Keagen <keegenjt@gmail.com>
2024-11-04 18:30:12 -0500

    WIP attempting to make recipes

--- baker.c
+++ baker.c
@@ -2,6 +2,28 @@
 
 void* create_baker(void* arg) {
   Baker* baker = (Baker*)arg;
+  
+  // Setup needed recipes, not sure about a better way tbh
+  Recipe cookies = {
+    ingredients = [
+      Ingredient {
+        name = "Flour",
+        location = baker->kitchen->pantry
+      },
+      Ingredient {
+        name = "Sugar",
+        location = baker->kitchen->pantry
+      },
+      Ingredient {
+        name = "Milk",
+        location = baker->kitchen->refridgerator
+      },
+      Ingredient {
+        name = "Butter",
+        location = baker->kitchen->refridgerator
+      }
+    ]
+  };
 
   sem_wait(&(baker->kitchen->mixer));
   printf("[%lu] - Using mixer\n", baker->id);
